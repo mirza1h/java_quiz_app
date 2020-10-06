4212e56d367ac37188ea51e944304fa4f5fa4f28
Mirza Hasanovic <mirza.hasanovic1@fet.ba>
2020-10-06 19:06:33 +0200

    js changes

--- WebContent/js/single_quiz.js
+++ WebContent/js/single_quiz.js
@@ -1,25 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
 	'use strict';
 	
-	// Set often needed elements global
+	// Set often-needed elements global
+	var playQuizBtn = document.getElementById("idPlayQuizBtn");
+	var questionText = document.getElementById("idQuestionText");
+	var questionAnswerDiv = document.getElementById("idQuestionAnswer");
+	var nextQuestionBtn = document.getElementById("idNextQuestion");
+	var mainQuestionsDiv = document.getElementById("idMainQuestionsDiv");
+	var answersDiv = document.getElementById("idAnswersDiv");
+	var submitBtn = document.getElementById("idSubmitBtn");
+	var resultText = document.getElementById("idResultText");
 	var questions;
 	var currentQuestion = 0;
 	var currentTimer;
 	var interval = 0.5;
 	var skippedQuestions = [];
-	var playQuizBtn = document.getElementById("idPlayQuizBtn");
-	var questionText = document.getElementById("idQuestionText");
-	var questionAnswerDiv = document.getElementById("idQuestionAnswer");
-	var skipQuestionBtn = document.getElementById("idSkipQuestion");
-	var nextQuestionBtn = document.getElementById("idNextQuestion");
-	var mainQuestionsDiv = document.getElementById("idMainQuestionsDiv");
-	var answersDiv = document.getElementById("idAnswersDiv");
 
 	var init = function () {
 		// Register 'click' callbacks for buttons
 		playQuizBtn.addEventListener("click", playQuiz);
-		skipQuestionBtn.addEventListener("click", skipQuestion);
 		nextQuestionBtn.addEventListener("click", nextQuestion);
+		submitBtn.addEventListener("click", submitResults);
 	}
 
 	function answerChecked(event) {
@@ -29,6 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	}
 
 	function playQuiz() {
+		document.getElementById('data').innerHTML = '';
 		var quizId = document.getElementById("idQuiz").value;
 
 		$.ajax({
@@ -44,9 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
 					questions[i].timeLeft = questions[i].time;
 					questions[i].checkedAnswers = [];
 				}
-
 				goToQuestion(currentQuestion);
-
 			}
 		});
 
@@ -70,18 +70,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		clearTimer();
 		getCheckedAnswers();
 		if (currentQuestion == questions.length - 1) {
-			goToUnansweredList();
-		} else {
-			goToQuestion(++currentQuestion);
-		}
-	}
-
-	function skipQuestion() {
-		clearTimer();
-		skippedQuestions.push(currentQuestion);
-
-		if (currentQuestion == questions.length - 1) {
-			goToUnansweredList();
+			goToResults();
 		} else {
 			goToQuestion(++currentQuestion);
 		}
@@ -97,15 +86,6 @@ document.addEventListener("DOMContentLoaded", function () {
 					timer.style.width = ((questions[currentQuestion].timeLeft / questions[currentQuestion].time) * 100)
 							.toString()
 							+ "%";
-
-					if (questions[currentQuestion].timeLeft < 11) {
-						timer.classList.remove("light-blue");
-						timer.classList.add("red");
-						document.getElementById("timer-outline").classList
-								.remove("teal");
-						document.getElementById("timer-outline").classList
-								.add("red");
-					}
 
 					if (questions[currentQuestion].timeLeft <= 0) {
 						clearTimer();
@@ -135,73 +115,6 @@ document.addEventListener("DOMContentLoaded", function () {
 		}
 	}
 
-	function goToUnansweredList() {
-
-		getCheckedAnswers();
-
-		clearTimer();
-
-		if (skippedQuestions.length == 0) {
-			goToResults();
-			return;
-		}
-
-		var unansweredList = '<div class="row transparent question-container">'
-				+ '<div class="container transparent col l8 offset-l2 m12 s12"> '
-				+ '<div class="quiz-question transparent"> <div class="question-box '
-				+ ' lighten-3 z-depth-3"> '
-				+ '<span class="text">You are finished, but you have skipped a few questions. Click them if you want to do them again.'
-				+ '</span> </div> ';
-
-		for ( var i in skippedQuestions) {
-			unansweredList += '<div class="answer-box  '
-					+ ' lighten-4 waves-effect waves-teal" onClick="goToSkippedQuestion('
-					+ skippedQuestions[i] + ')"> ' + '<span class="text">Question '
-					+ (skippedQuestions[i] + 1) + '</span> </div> ';
-		}
-
-		unansweredList += '<div> <button href="#" class="btn black-text btn-flat '
-				+ ' lighten-3 right" onclick="goToResults()">Finish</button> </div>'
-				+ '</div> </div> </div> ';
-
-		document.getElementById('data').innerHTML = unansweredList;
-	}
-
-	function goToSkippedQuestion(questionNumber) {
-
-		for (var i = 0; i < skippedQuestions.length; i++) {
-			if (skippedQuestions[i] == questionNumber) {
-				skippedQuestions.splice(i, 1);
-			}
-		}
-
-		var questionString = '<div class="row transparent question-container">'
-				+ '<div class="container transparent col l8 offset-l2 m12 s12">'
-				+ ' <div class="quiz-question transparent" id="wholeQuestion"> <div class="question-box '
-				+ ' lighten-3 z-depth-3">' + ' <span class="text">'
-				+ questions[questionNumber].text + '</span> </div>';
-
-		for ( var a in questions[questionNumber].answers) {
-			questionString += '<div class="answer-box  '
-					+ ' lighten-4 waves-effect waves-teal" onClick="answerChecked(this)">'
-					+ ' <input type="checkbox"> <span class="text">'
-					+ questions[questionNumber].answers[a].text + '</span> </div> ';
-		}
-
-		questionString += ' <div class="progress teal lighten-4" id="timer-outline">'
-				+ ' <div class="determinate light-blue darken-2" id="timer" style="width: 100%"></div>'
-				+ ' </div> <div>'
-				+ ' <a href="#" class="btn right '
-				+ ' lighten-2 black-text" onclick="goToUnansweredList()"><i class="material-icons right">keyboard_arrow_right</i>List</a>'
-				+ ' </div> </div> </div> </div> ';
-
-		document.getElementById('data').innerHTML = questionString;
-
-		currentQuestion = questionNumber;
-
-		setUpTimer();
-	}
-
 	function goToResults() {
 		var quizId = document.getElementById("idQuiz").value;
 
@@ -223,46 +136,9 @@ document.addEventListener("DOMContentLoaded", function () {
 	}
 
 	function printResults(result) {
-
-		var resultsString = '<div class="row transparent question-container"> '
-				+ '<div class="container transparent col l8 offset-l2 m12 s12"> '
-				+ '<div class="question-box white lighten-3 z-depth-3"> <div class="quiz-question white"> '
-				+ '<div class="text">Congratulations, your score is '
-				+ result
-				+ '%. Please submit your results. </div> </div> '
-				+ '<div class="input-field"> <input id="first_name" type="text" class="validate"> <label for="first_name">First Name</label>'
-				+ ' </div> <div class="input-field"> <input id="last_name" type="text" class="validate"> <label for="last_name">Last Name</label>'
-				+ ' </div> <div class="input-field"> <input id="email" type="email" class="validate"> <label for="email">Email</label> '
-				+ '</div> <div> <button href="#" class="btn black-text btn-flat teal lighten-3 right" onclick="submitResults('
-				+ result + ')">Send</button> ' + '</div> </div> </div> </div> ';
-
-		console.log(questions);
-		document.getElementById('data').innerHTML = resultsString;
-	}
-
-	function submitResults(result) {
-		var quizId = document.getElementById("idQuiz").value;
-		var firstName = $('#first_name').val();
-		var lastName = $('#last_name').val();
-		var email = $('#email').val();
-
-		var quizResults = {};
-		quizResults.questions = questions;
-
-		$.ajax({
-			url : 'score-servlet',
-			type : 'POST',
-			data : {
-				'quizId' : quizId,
-				'firstName' : firstName,
-				'lastName' : lastName,
-				'email' : email,
-				'result' : result
-			},
-			success : function(result) {
-				window.location.href = "<%=request.getContextPath()%>/";
-			}
-		});
+		mainQuestionsDiv.style.display = "none";
+		resultText.style.display = "block";
+		resultText.innerHTML = 'Your score is: ' + result + '.'
 	}
 
 
--- WebContent/single_quiz.jsp
+++ WebContent/single_quiz.jsp
@@ -18,9 +18,6 @@
 <!-- Compiled and minified CSS -->
 <link rel="stylesheet"
 	href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-<!-- Material icons -->
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-	rel="stylesheet">
 <!-- Bootstrap CSS -->
 <link rel="stylesheet"
 	href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
@@ -35,38 +32,20 @@
 }
 
 .question-box {
-	background: red;
+	background: gray;
 	padding: 20px;
 	margin: 20px;
 	font-size: 150%;
 	border-radius: 4px;
 }
 
-.answer-box {
-	width: 80%;
-	margin: 2em;
-	display: block;
-	border-radius: 4px;
-	padding: 1em;
-	cursor: pointer;
-}
 </style>
 </head>
-
-<%!Quiz getQuiz(String quizId) {
-
-		QuizService quizService = new QuizService(new QuizDao());
-
-		Quiz quiz = quizService.findById(Integer.parseInt(quizId));
-
-		return quiz;
-
-	}%>
-
 <body>
 
 	<%
-		Quiz quiz = getQuiz(request.getParameter("quiz-id"));
+		QuizService quizService = new QuizService(new QuizDao());
+		Quiz quiz = quizService.findById(Integer.parseInt(request.getParameter("quiz-id")));
 	%>
 
 
@@ -96,55 +75,48 @@
 		<div class="container transparent row">
 			<div class="container col l8 offset-l2 m10 offset-m1 s12">
 				<div style="position: relative;">
-					<div id="background" class="bg-image"
-						style="background-image: url(<%=quiz.getImageUrl()%>);"></div>
-
 					<div class="image">
 						<img class="image" src="<%=quiz.getImageUrl()%>" alt="">
 					</div>
-
 				</div>
-
 				<h3><%=quiz.getTitle()%></h3>
 				<span><%=quiz.getDescription()%></span>
-
 				<div id="idPlayQuizBtn" class="begin-button right ">
 					<a class="btn black-text lighten-2">Begin</a>
 				</div>
 			</div>
-
 		</div>
+	</div>
+	
+	<div id="idMainQuestionsDiv" class="row transparent question-container" style="display:none">
+		<div class="container">
+			<div class="quiz-question transparent" id="wholeQuestion">
+				<div class="question-box">
+					<span id="idQuestionText" class="text"> </span>
+				</div>
+				
+				<div id="idAnswersDiv">
+					<div id="idQuestionAnswer" class="btn btn-info" style="display:none">
+						<input type="checkbox" class="input-dim prt-2"/>
+						<span class="text"> </span>
+					</div>
+				</div>
 
-		<div id="idMainQuestionsDiv" class="row transparent question-container" style="display:none">
-			<div class="container transparent col l8 offset-l2 m12 s12">
-				<div class="quiz-question transparent" id="wholeQuestion">
-					<div class="question-box ighten-3 z-depth-3">
-						<span id="idQuestionText" class="text"> </span>
+				<div class="progress lighten-4" id="timer-outline">
+					<div class="determinate light-blue darken-2" id="timer"
+						style="width: 100%">
 					</div>
-					
-					<div id="idAnswersDiv">
-						<div id="idQuestionAnswer" class="answer-box lighten-4 waves-effect waves-teal" style="display:none">
-							<input type="checkbox" class="input-dim prt-2"/>
-							<span class="text"> </span>
-						</div>
-					</div>
-
-					<div class="progress teal lighten-4" id="timer-outline">
-						<div class="determinate light-blue darken-2" id="timer"
-							style="width: 100%">
-						</div>
-					</div>
-					<div>
-						<a id="idSkipQuestion" href="#" class="btn lighten-2 black-text"> 
-							<i class="material-icons right">skip_next</i>Skip
-						</a>
-						<a id="idNextQuestion" href="#" class="btn right lighten-2 black-text"> 
-							<i class="material-icons right">keyboard_arrow_right</i>Next question
-						</a>
-					</div>
+				</div>
+				<div>
+					<a id="idNextQuestion" href="#" class="btn right lighten-2 black-text"> 
+						Next question
+					</a>
 				</div>
 			</div>
 		</div>
+	</div>
+	<div id="idResultText" class="question-box" style="display:none">
+		<span class="text"> </span>
 	</div>
 	<!-- Compiled and minified JavaScript -->
 	<script
